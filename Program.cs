5e4b95d15f9828820f5b856997f2c43fa0f9e68d
Oleg <schika145@gmail.com>
2023-10-24 20:50:44 +0500

    Попытка начального ввода массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив из вещественных чисел с ненулевой дробной частью. Найдите разницу между максимальным и минимальным
+// элементом массива.
+Console.WriteLine("Введите размер массива");
+int Length = Convert.ToInt32(Console.ReadLine());
+
+double[] array = new double[Length];
+
+for (int i = 0; i < Length; i++)
+{
+    array[i] = new Random().Next(1, 10);
+    Console.Write(array[i] + " ");
+}
